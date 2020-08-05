7395c9d74fd4bf465f442fbfdbc65b6a985a8125
Anthony Piron <anthony.piron@ulb.ac.be>
2020-08-05 19:47:45 +0200

    Add macrophage

--- src/rrho.c
+++ src/rrho.c
@@ -129,7 +129,7 @@ rrho_hyper(struct rrho *rrho, size_t i, size_t j, struct rrho_result *res)
 {
   // double stats_hyper_F(long k, long K, long n, long N)
   size_t count = count_intersect(rrho, i, j);
-  
+  // double mean = (double) (i+1) * (double) (j+1) / rrho->n;
   
   res->pvalue = 1.0 - stats_hyper_F(count, i+1, j+1, rrho->n);
   // res->fdr = (0 == count)?-1:mean / count;
