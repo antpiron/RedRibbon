c6b35378e32fbdc0839827c7ab2ba8fa16b77c44
Anthony Piron <anthony.piron@ulb.ac.be>
2020-11-14 14:30:15 +0100

    Add benchmark for long double

--- perf/p-longdouble.c
+++ perf/p-longdouble.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <time.h>
+#include <float.h>
 
 #include "ale.h"
 #include "rrho.h"
@@ -16,21 +17,8 @@ main(int argc, char *argv[argc])
   struct timespec st, et;
 
   printf("%s: \n", argv[0]);
-
-  clock_gettime(CLOCK_REALTIME, &st);
-
-  resl = 0;
-  for (size_t i = 0 ; i < ITER ; i++)
-    {
-      resl += 11.0;
-      resl /= 3.0;
-    }
-      
-  clock_gettime(CLOCK_REALTIME, &et);
-  diff = (et.tv_sec - st.tv_sec) + (et.tv_nsec - st.tv_nsec) / 1e9d;
-  rate = ITER / diff ;
-  
-  printf("long double %12.2F sec, %12.0F operation/sec (%Le)\n", diff, rate, resl);
+  printf("sizeof(double)      = %d,  DBL_MIN = %e\n", sizeof(double), DBL_MIN);
+  printf("sizeof(long double) = %d, LDBL_MIN = %Le \n", sizeof(long double), LDBL_MIN);
 
   clock_gettime(CLOCK_REALTIME, &st);
 
@@ -47,6 +35,21 @@ main(int argc, char *argv[argc])
   
   printf("     double %12.2F sec, %12.0F operation/sec (%e)\n", diff, rate, res);
   
+  clock_gettime(CLOCK_REALTIME, &st);
+
+  resl = 0;
+  for (size_t i = 0 ; i < ITER ; i++)
+    {
+      resl += 11.0;
+      resl /= 3.0;
+    }
+      
+  clock_gettime(CLOCK_REALTIME, &et);
+  diff = (et.tv_sec - st.tv_sec) + (et.tv_nsec - st.tv_nsec) / 1e9d;
+  rate = ITER / diff ;
   
+  printf("long double %12.2F sec, %12.0F operation/sec (%Le)\n", diff, rate, resl);
+
+ 
   return EXIT_SUCCESS;
 }
