4557eb0449cb6c287ed74b1190d35e84e0d40767
Anthony Piron <anthony.piron@ulb.ac.be>
2021-02-18 13:06:48 +0100

    Fix perf test

--- perf/p-rrho-permutation-ea.c
+++ perf/p-rrho-permutation-ea.c
@@ -84,42 +84,7 @@ main(int argc, char *argv[argc])
       free(b);
     }
 
-  /* from RRHO paper
-     For a comparison of gene lists of length 5000 and a rank threshold step size of 50, it takes ∼15 s to calculate the 10 000 [(5000/50)2] hypergeometric CDF results required to create one RRHO map using an Intel Xeon 3.2 GHz processor, thus requiring ∼4 h to create and analyze 1000 permutation maps on a single computational node.
-  */
-  size_t vec_n =  5000;
-  double *a = malloc(vec_n * sizeof(double));
-  double *b = malloc(vec_n * sizeof(double));
-  size_t m = 100, n = 100;
-  struct rrho_rectangle_params_ea params_ea;
-  
-  for (size_t i = 0 ; i < vec_n ; i++)
-    {
-      a[i] = stats_unif_std_rand();
-      b[i] = stats_unif_std_rand();
-    }
-
-  rrho_init(&rrho, vec_n, a, b);    
-      
-  clock_gettime(CLOCK_REALTIME, &st);
-
-  rrho_init_params_ea(&rrho, &params_ea);
-  rrho_rectangle_min_ea(&rrho, 0, 0, vec_n, vec_n, &params_ea, RRHO_HYPER, 1, &rrho_coord);
-  rrho_hyper(&rrho, rrho_coord.i, rrho_coord.j, &rrho_res);
-  rrho_permutation_generic(&rrho, 0, 0, vec_n, vec_n, &params_ea, RRHO_HYPER, 1, RRHO_EA, 96, rrho_res.pvalue, &perm_res);
-  
-  clock_gettime(CLOCK_REALTIME, &et);
-  diff = (et.tv_sec - st.tv_sec) + (et.tv_nsec - st.tv_nsec) / 1e9d;
-  
-  rrho_destroy(&rrho);
-
-  printf("RRHO for vectors of n = %6zu elements, step = %4ld, n.threads = %3d: ", vec_n, vec_n / n, omp_get_max_threads());
-  printf("%12.2F sec (pvalue = %Le, padj = %Le)\n", diff, rrho_res.pvalue, perm_res.pvalue);
-  fprintf(file, "%d\t%zu\t%zu\t%e\n", omp_get_max_threads(), vec_n, vec_n / n, diff); 
-
   fclose(file);
-  free(a);
-  free(b);
 
   return EXIT_SUCCESS;
 }
